710b4361b3a79056754e8eb51f80de1ddc3fee94
philippe124 <philippe.latour1997@hotmail.com>
2016-05-02 14:58:19 -0400

    Add roi

---
+++ JeuEchec/Librairie/Pieces/Roi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace JeuEchec.Librairie.Pieces
+{
+    class Roi : Piece
+    {
+        #region Propriétés
+
+        protected override Image imageBlanc { get { return Properties.Resources.blanc_roi; } }
+        protected override Image imageNoir { get { return Properties.Resources.noir_roi; } }
+
+        #endregion
+
+        #region Constructeurs
+
+        public Roi(Position position, Couleurs couleur) : base(position, couleur)
+        {
+
+        }
+
+        #endregion
+
+        #region Méthodes
+
+        public override Position[] ObtenirPosibilites()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
