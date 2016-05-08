effa83083209b17a0e4310751014600748208934
philippe124 <philippe.latour1997@hotmail.com>
2016-05-08 18:53:59 -0400

    Add TcpIp Project

--- TCPIP/Parametre.cs
+++ TCPIP/Parametre.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace TCPIP
 {
@@ -19,18 +22,26 @@ namespace TCPIP
 
         #endregion
 
+        #region Constantes
+
+         private const string FICHIER = "ParametreTCPIP.xml";
+
+        #endregion
+
+        #region Champs
+
+        private string adresseIp = "";
+        private int port = 0;
+
+        #endregion
+
         #region Properties
 
-
-
-
-        private string ipAddress = "";
-        private int port = 0;
-        public string IpAddress
+        public string AdresseIp
         {
             get
             {
-                return ipAddress;
+                return adresseIp;
             }
 
             set
@@ -38,9 +49,10 @@ namespace TCPIP
                 Regex rgx = new Regex(@"^([01]?\d{1,2}|2[0-4]\d|25[0-5])\.([01]?\d{1,2}|2[0-4]\d|25[0-5])\.([01]?\d{1,2}|2[0-4]\d|25[0-5])\.([01]?\d{1,2}|2[0-4]\d|25[0-5])$");
                 if (!rgx.IsMatch(value)) value = "";
 
-                ipAddress = value;
+                adresseIp = value;
             }
         }
+
         public int Port
         {
             get
@@ -55,7 +67,46 @@ namespace TCPIP
                 port = value;
             }
         }
+
         public Modes Mode { get; set; }
+
+        #endregion
+
+        #region Constructeurs
+
+        public Parametre(string adresseIp, int port, Modes modes)
+        {
+            AdresseIp = adresseIp;
+            Port = port;
+            Mode = modes;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Sauvegarder()
+        {
+            FileStream file = new FileStream(FICHIER, FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(Parametre));
+            serializer.Serialize(file, this);
+            file.Close();
+        }
+
+        public static Parametre Charger()
+        {
+            Parametre parametre = null;
+
+            if (File.Exists(FICHIER))
+            {
+                FileStream file = new FileStream(FICHIER, FileMode.Open);
+                XmlSerializer serializer = new XmlSerializer(typeof(Parametre));
+                parametre = ((Parametre)serializer.Deserialize(file));
+                file.Close();
+            }
+
+            return parametre;
+        }
 
         #endregion
     }
--- TCPIP/Poste.cs
+++ TCPIP/Poste.cs
@@ -9,9 +9,50 @@ namespace TCPIP
 {
     public class Poste
     {
+        #region Champs
+
+        Parametre _parametre;
+        ClientTcpIp _client;
+        ServeurTcpIp _serveur;
+
+        #endregion
+
+        #region Constructeurs
+
         public Poste(Parametre parametre)
         {
-
+            _parametre = parametre;
         }
+
+        #endregion
+        #region Proprietes
+
+        public delegate void RecevoirMessage(object donnee);
+
+        #endregion
+
+        #region Methods
+
+        public void AttendreConnection()
+        {
+            if (_parametre.Mode == Parametre.Modes.Client)
+            {
+                _client = new ClientTcpIp();
+                _client.NotifierMessages += IntercepterMessage;
+                _client.Connecter(_parametre.AdresseIp, _parametre.Port);
+            }
+            else if (_parametre.Mode == Parametre.Modes.Serveur)
+            {
+                _serveur = new ServeurTcpIp();
+                _serveur.Écouter(_parametre.AdresseIp, _parametre.Port);
+            }
+        }
+
+        public void IntercepterMessage(object sender, MessageArgs e)
+        {
+            object obj = new object();
+            
+        }
+        #endregion
     }
 }
