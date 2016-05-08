8a09cd7b9d42dd1deb2fa403159d17603b56c4e0
philippe124 <philippe.latour1997@hotmail.com>
2016-05-08 17:07:49 -0400

    Add Network class

---
+++ TCPIP/Parametre.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TCPIP
+{
+    public class Parametre
+    {
+        #region Enumerations
+
+        public enum Modes
+        {
+            Client,
+            Serveur
+        }
+
+        #endregion
+
+        #region Properties
+
+
+
+
+        private string ipAddress = "";
+        private int port = 0;
+        public string IpAddress
+        {
+            get
+            {
+                return ipAddress;
+            }
+
+            set
+            {
+                Regex rgx = new Regex(@"^([01]?\d{1,2}|2[0-4]\d|25[0-5])\.([01]?\d{1,2}|2[0-4]\d|25[0-5])\.([01]?\d{1,2}|2[0-4]\d|25[0-5])\.([01]?\d{1,2}|2[0-4]\d|25[0-5])$");
+                if (!rgx.IsMatch(value)) value = "";
+
+                ipAddress = value;
+            }
+        }
+        public int Port
+        {
+            get
+            {
+                return port;
+            }
+
+            set
+            {
+                if (value < 0 || value >= 65535) value = 0;
+
+                port = value;
+            }
+        }
+        public Modes Mode { get; set; }
+
+        #endregion
+    }
+}
---
+++ TCPIP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("TCPIP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TCPIP")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("e54879b3-4cb3-4b8a-ba33-c9c892c8faf7")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
